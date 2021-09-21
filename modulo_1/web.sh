7dce7ba82944ded27b033a8af0f192a6ad93df98
calamarzo <calamarzo@users.noreply.github.com>
2021-09-21 10:57:20 +0200

    Create web.sh
    pasamos por parámetros web y palabra a buscar (sin fichero)

---
+++ modulo_1/web.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+        echo "Dame una web en formato entero (ej.: https://lemoncode.net)"
+        read WEB
+else
+        WEB=$1
+fi
+
+if [ -z "$2" ]
+then
+        echo "Dame una palabra:"
+        read WORD
+else
+        WORD=$2
+fi
+curl -v --silent $WEB --stderr - | grep -n $WORD | awk -F: '{print "Linea : "$1}'
